57605da7e348fa6dc77fc0d5c0cbe9ce2204cbbf
Michael Ibuaku <mujiangelo17@gmail.com>
2023-10-16 07:29:20 +0300

    lets see

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -3,22 +3,25 @@
 import { Stack, Box, Heading, Text, Img } from "@chakra-ui/react";
 
 const Hero = () => {
-    return ( 
+    return (
         <Stack
-        width={'100%'}
-        minH={'80vh'}
-        backgroundImage={'/15pro.png'}
-        backgroundSize={'cover'}
-        backgroundPosition={'center center'}
+            width={'100%'}
+            minH={'80vh'}
+            color={'white'}
+            backgroundImage={'/15pro.png'}
+            backgroundSize={'cover'}
+            backgroundPosition={'center center'}
         >
-            <Heading>iPhone 15 Pro</Heading>
-            <Text>Titanium. So strong. So light. So pro.</Text>
-            <Stack color={'blue'}>
-                <Text>Learn More </Text>
-                <Text>Buy</Text>
-            </Stack>
+            <Box className="heroContent" textAlign={'center'}>
+                <Heading>iPhone 15 Pro</Heading>
+                <Text>Titanium. So strong. So light. So pro.</Text>
+                <Stack direction={'row'} color={'blue'}>
+                    <Text>Learn More </Text>
+                    <Text>Buy</Text>
+                </Stack>
+            </Box>
         </Stack>
-     );
+    );
 }
- 
+
 export default Hero;
