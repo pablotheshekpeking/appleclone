90180d7f2fd916dfdd805cd8e11de13790f90f49
Michael Ibuaku <mujiangelo17@gmail.com>
2023-10-16 13:23:13 +0300

    letss

--- src/app/providers.js
+++ src/app/providers.js
@@ -10,7 +10,7 @@ export function Providers({ children }) {
   return (
     <CacheProvider>
       <ChakraProvider>
-        <Navbar />
+        <Navbar style={{postion: 'fixed', top:'0px'}} />
 
         <Section1 />
 
