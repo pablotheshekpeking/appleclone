f428c35fad1abc131bd96eb981c2a1e462f88c96
Michael Ibuaku <mujiangelo17@gmail.com>
2023-10-21 12:01:29 +0300

    test 1

--- src/app/components/row2.js
+++ src/app/components/row2.js
@@ -8,14 +8,10 @@ const Row2 = () => {
             direction={['column', 'column', 'row', 'row']}
             w={'100%'}
         >
+
             <Box w={['100%', '100%', '50%', '50%']} m={'10px'}>
-                <Stack
-                    minH={'66vh'}
-                    backgroundImage={'/mac.jpg'}
-                    backgroundSize={'cover'}
-                    backgroundPosition={'center center'}
-                >
-                    <Box className="heroContent" textAlign={'center'} pt={'70px'}>
+                <>
+                    <Box className="heroContent" textAlign={'center'} pt={'70px'} bg={'#FAFAFA'}>
                         <Heading fontSize={'30px'}>MacBook Air 15"</Heading>
                         <Text fontSize={['', '', '25px', '25px']}>Impressively big. Impressively thin.</Text>
                         <Stack direction={'row'} color={'#2A97FB'} justify={'center'} fontSize={['', '', '20px', '20px']}>
@@ -23,25 +19,22 @@ const Row2 = () => {
                             <Text cursor={'pointer'} _hover={{ textDecoration: 'underline', }}>Buy</Text>
                         </Stack>
                     </Box>
-                </Stack>
+                    <Img src="/mac.jpg" w={'100%'} h={'auto'} mb={'10px'} />
+                </>
             </Box>
 
             <Box w={['100%', '100%', '50%', '50%']} m={'10px'}>
-                <Stack
-                    minH={'66vh'}
-                    backgroundImage={'/airpods.jpeg'}
-                    backgroundSize={'cover'}
-                    backgroundPosition={'center center'}
-                >
-                    <Box className="heroContent" textAlign={'center'} pt={'70px'}>
-                        <Heading fontSize={'30px'}>AirPods Pro</Heading>
+                <>
+                    <Box className="heroContent" textAlign={'center'} pt={'70px'} bg={'black'} color={'white'}>
+                    <Heading fontSize={'30px'}>AirPods Pro</Heading>
                         <Text fontSize={['', '', '25px', '25px']}>Adaptive audio. Now playing.</Text>
                         <Stack direction={'row'} color={'#2A97FB'} justify={'center'} fontSize={['', '', '20px', '20px']}>
                             <Text cursor={'pointer'} _hover={{ textDecoration: 'underline', }}>Learn More </Text>
                             <Text cursor={'pointer'} _hover={{ textDecoration: 'underline', }}>Buy</Text>
                         </Stack>
                     </Box>
-                </Stack>
+                    <Img src="/airpods.jpeg" w={'100%'} h={'auto'} mb={'10px'} />
+                </>
             </Box>
         </Stack>
     );
