bd04d8e640ce5fd82d1334ecb93a26d7ea24afa6
Michael Ibuaku <mujiangelo17@gmail.com>
2023-10-21 11:34:15 +0300

    again

--- src/app/components/row1.js
+++ src/app/components/row1.js
@@ -30,7 +30,7 @@ const Row1 = () => {
                     backgroundSize={'cover'}
                     backgroundPosition={'center center'}
                 >
-                    <Box className="heroContent" textAlign={'center'} pt={'170px'}>
+                    <Box className="heroContent" textAlign={'center'} pt={'320px'}>
                         <Heading fontSize={'30px'}>Vision Pro</Heading>
                         <Text fontSize={['', '', '25px', '25px']}>Welcome to the era of spacial computing</Text>
                         <Text color={'#dddddd'}>Availabe early next year in the U.S</Text>
